b5beb61f80b1abc93fd59efe28b9b2af85b55a15
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-07-15 05:34:26 +0300

    Add operators for Functor and Applicative stuff

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 
-// Free JSON value representation type
+// Free JSON value representation type {{{1
 
 struct JsonValue; // Algebraic data type
 
@@ -26,8 +26,12 @@ struct Null: tuple<> {};
 
 struct JsonValue: variant<Object, Array, String, Number, Bool, Null> {};
 
+// }}}1
 
-// Basic parser implementation
+
+// Basic parser implementation {{{1
+
+// Basic type {{{2
 
 using Input = string;
 struct ParsingError: string {};
@@ -42,7 +46,10 @@ template <typename A>
 // Parser a = String → Either String (a, String)
 struct Parser: function<ParsingResult<A>(Input)> {};
 
-// Functor-like stuff
+// }}}2
+
+// Functor-like stuff {{{2
+
 template <typename A, typename B>
 // (<$>) :: (a → b) → Parser a → Parser b
 Parser<B> fmapParser(function<B(A)> mapFn, Parser<A> parser)
@@ -62,24 +69,54 @@ Parser<B> fmapParser(function<B(A)> mapFn, Parser<A> parser)
 	}};
 }
 template <typename A, typename B>
+// Operator equivalent for “fmapParser”
+Parser<B> operator|(function<B(A)> mapFn, Parser<A> parser)
+{
+	return fmapParser<A, B>(mapFn, parser);
+}
+template <typename A, typename B>
+// Flipped version of “fmap” (like (<&>) comparing to (<$>))
+Parser<B> operator&(Parser<A> parser, function<B(A)> mapFn)
+{
+	return fmapParser<A, B>(mapFn, parser);
+}
+
+template <typename A, typename B>
 // (<$) :: a → Parser b → Parser a
 Parser<A> voidRight(A toValue, Parser<B> parser)
 {
 	return fmapParser<B, A>([=](B) { return toValue; }, parser);
 }
 template <typename A, typename B>
+// Operator equivalent for “voidRight”
+Parser<A> operator<=(A toValue, Parser<B> parser)
+{
+	return voidRight<A, B>(toValue, parser);
+}
+
+template <typename A, typename B>
 // ($>) :: Parser a → b → Parser b
 Parser<B> voidLeft(Parser<A> parser, B toValue)
 {
 	return voidRight(toValue, parser);
 }
+template <typename A, typename B>
+// Operator equivalent for “voidLeft”
+Parser<B> operator>=(Parser<A> parser, B toValue)
+{
+	return voidLeft<A, B>(parser, toValue);
+}
 
-// Applicative-like stuff
+// }}}2
+
+// Applicative-like stuff {{{2
+
 template <typename A>
 Parser<A> pure(A x)
 {
 	return Parser<A>{[=](Input input) { return make_tuple(x, input); }};
 }
+
 template <typename A, typename B>
 // (<*>) :: Parser (a → b) → Parser a → Parser b
 Parser<B> apply(Parser<function<B(A)>> fnParser, Parser<A> parser)
@@ -95,6 +132,13 @@ Parser<B> apply(Parser<function<B(A)>> fnParser, Parser<A> parser)
 	}};
 }
 template <typename A, typename B>
+// Operator equivalent for “apply”
+Parser<B> operator^(Parser<function<B(A)>> fnParser, Parser<A> parser)
+{
+	return apply<A, B>(fnParser, parser);
+}
+
+template <typename A, typename B>
 // (<*) :: Parser a → Parser b → Parser a
 Parser<A> applyFirst(Parser<A> parserA, Parser<B> parserB)
 {
@@ -108,6 +152,13 @@ Parser<A> applyFirst(Parser<A> parserA, Parser<B> parserB)
 	);
 }
 template <typename A, typename B>
+// Operator equivalent for “applyFirst”
+Parser<A> operator<<(Parser<A> parserA, Parser<B> parserB)
+{
+	return applyFirst<A, B>(parserA, parserB);
+}
+
+template <typename A, typename B>
 // (*>) :: Parser a → Parser b → Parser b
 Parser<B> applySecond(Parser<A> parserA, Parser<B> parserB)
 {
@@ -120,6 +171,16 @@ Parser<B> applySecond(Parser<A> parserA, Parser<B> parserB)
 		parserB
 	);
 }
+template <typename A, typename B>
+// Operator equivalent for “applySecond”
+Parser<B> operator>>(Parser<A> parserA, Parser<B> parserB)
+{
+	return applySecond<A, B>(parserA, parserB);
+}
+
+// }}}2
+
+// Parsing functions {{{2
 
 template <typename A, typename B>
 // either :: (a → c) → (b → c) → Either a b → c
@@ -158,6 +219,10 @@ variant<ParsingError, A> parse(Parser<A> parser, Input input)
 	);
 }
 
+// }}}2
+
+// }}}1
+
 
 int test_basic_stuff();
 
@@ -175,24 +240,37 @@ int test_basic_stuff()
 	}};*/
 	const Parser<int> test1 = pure(123);
 
-	const Parser<string> test2 = fmapParser<int, string>([](int x) {
-		return to_string(x);
-	}, test1);
+	/* const Parser<string> test2 = fmapParser<int, string>([](int x) { */
+	/* 	return to_string(x); */
+	/* }, test1); */
+	const Parser<string> test2 =
+		function<string(int)>([](int x) { return to_string(x); }) | test1;
 
-	const Parser<bool> test3 = voidLeft<string, bool>(test2, true);
+	const Parser<bool> test3 = test2 >= true;
 
+	/* const Parser<function<string(bool)>> test4 = */
+	/* 	voidRight<function<string(bool)>, bool>( */
+	/* 		[](bool x) { return x ? "yes" : "no"; }, */
+	/* 		test3 */
+	/* 	); */
 	const Parser<function<string(bool)>> test4 =
-		voidRight<function<string(bool)>, bool>(
-			[](bool x) { return x ? "yes" : "no"; },
-			test3
-		);
+		function<string(bool)>([](bool x) { return x ? "yes" : "no"; })
+			<= test3;
 
-	const Parser<string> test5 = apply<bool, string>(test4, test3);
-	const Parser<int> test6 = applyFirst<int, string>(test1, test5);
-	const Parser<string> test7 = applySecond<int, string>(test6, test5);
+	/* const Parser<string> test5 = apply<bool, string>(test4, test3); */
+	const Parser<string> test5 = test4 ^ test3;
+
+	/* const Parser<int> test6 = applyFirst<int, string>(test1, test5); */
+	const Parser<int> test6 = test1 << test5;
+
+	/* const Parser<string> test7 = applySecond<int, string>(test6, test5); */
+	const Parser<string> test7 = test6 >> test5;
+
+	const Parser<string> test8 =
+		test7 & function<string(string)>([](string x) { return ">"+x+"<"; });
 
 	using T = string;
-	const variant<ParsingError, T> result = parse<T>(test7, "foobar");
+	const variant<ParsingError, T> result = parse<T>(test8, "foobar");
 
 	if (auto x = get_if<ParsingError>(&result)) {
 		cerr << "Failed to parse: " << *x << endl;
